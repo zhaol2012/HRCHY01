e44c4485335264c69b04038d9686bbb1b1e0f25e
“zhaol2012git config --global user.email zhaol2012@gmail.com <anders.zhao@sap.com>
2019-08-26 15:15:56 +0800

    Hierarchy Testing First Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function(Controller) {
+    "use strict";
+
+    return Controller.extend("anders.aif.if.overview.controller.App", {
+
+        onInit : function() {
+            //nothing to do for us
+        }
+
+    });
+
+});
